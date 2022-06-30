e16a9a92b607346372b0d8a48b76a9ddab3c6c11
dadoffreya <ardianfirman.139@gmail.com>
2022-06-30 07:59:59 +0700

    Update InspectApp.js

--- frontend/src/components/pages/InspectApp.js
+++ frontend/src/components/pages/InspectApp.js
@@ -33,6 +33,7 @@ const InspectApp = () => {
     const rftRate = counttotalgradea / (counttotalgradea + counttotalgradeb) * 100;
     const rftRatemath = Math.round(rftRate);
     const [linenumber, setLineNumber] = useState("Line 1");
+    const [defectcode, setDefectCode] = useState(null);
 
     // POST OK VALUE
     const lineHandler = (event) => {
@@ -55,11 +56,25 @@ const InspectApp = () => {
             console.log(error);
         }
     };
-    
-    const getData = async ()=>{
-        const response = await axios.get("http://localhost:5000/passes");
-        console.log(response.data);
-    }
+    const addDefect = async (e) => {
+        setDefectCode();
+        e.preventDefault();
+        try {
+            await axios.post("http://localhost:5000/passes",{
+                "line": linenumber,
+                "status": "Defect",
+                "code": e.target.value,
+                "qty": qty
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // const getData = async ()=>{
+    //     const response = await axios.get("http://localhost:5000/passes");
+    //     console.log(response.data);
+    // }
 
   return (
     <div>
@@ -491,7 +506,8 @@ const InspectApp = () => {
                                     <Button 
                                         variant="contained" 
                                         key={opsi.nomor} 
-                                        onClick={() => setTotalGradeB(counttotalgradeb + 1)}
+                                        value={defectcode}
+                                        onClick={addDefect}
                                         sx={{
                                                 m: 1,
                                                 width: '15rem',
