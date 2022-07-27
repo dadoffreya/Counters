81bb697b603ce175745b4d6a27c098968b06e88a
dadoffreya <ardianfirman.139@gmail.com>
2022-07-27 10:16:01 +0700

    add login

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 import express from "express";
 import cors from "cors";
-import Routes from "./routes/Routes.js"
-
+import dbase from "./config/Database.js";
+import Routes from "./routes/Routes.js";
+import Users from "./models/UserModel.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+try {
+    await dbase.authenticate();
+    console.log('Database tersambung...');
+    await Users.sync();
+} catch (error) {
+    console.error(error);
+}
+
 app.use(cors());
 app.use(express.json());
 app.use(Routes);
--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getPassed, addPassed, getOK, countOK, countDefect } from "../controller/PassedController.js";
 import { getIssues, addIssues } from "../controller/IssueController.js";
+import { getUsers, Register, Login } from "../controller/Users.js";
 
 const router = express.Router();
 
@@ -11,5 +12,9 @@ router.get('/rft/ok', getOK);
 router.get('/rft/countok', countOK);
 router.get('/rft/countdef', countDefect);
 router.post('/rft/issues', addIssues);
+router.get('/users', getUsers);
+router.post('/users', Register);
+router.post('/login', Login);
+
 
 export default router;
