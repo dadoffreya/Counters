76430d86ddc2b0b61dfbf35ee657d8aaf45a1255
dadoffreya <ardianfirman.139@gmail.com>
2022-08-15 16:54:38 +0700

    Add Tabs Panel for Charts Page

--- frontend/src/components/pages/ChartsPage.js
+++ frontend/src/components/pages/ChartsPage.js
@@ -1,11 +1,77 @@
 import React from 'react';
 import Navbar from '../Navbar';
 import Grid from '@mui/material/Grid';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import RFTchart from './RFTchart';
+
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
 
 const ChartsPage = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
   return (
     <div>
         <Navbar />
+        <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            sx={{ borderRight: 1, borderColor: 'divider' }}
+        >
+            <Tab label="RFT Charts" {...a11yProps(0)} />
+            <Tab label="Performance Charts" {...a11yProps(1)} />
+            <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+            <RFTchart />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+            Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+            Halu
+        </TabPanel>
         <Grid 
             container
             spacing={0}
@@ -20,16 +86,16 @@ const ChartsPage = () => {
             <Grid 
                 item
             >
-                <iframe 
+                {/* <iframe 
                     width="900" 
                     height="695" 
                     src="https://datastudio.google.com/embed/reporting/5dc4da61-1505-4a7f-af2d-1c983d1810d9/page/7AB0C" 
-                    frameborder="10" 
+                    frameborder="5" 
                     allowfullscreen
                     title="Charts" 
-                    >
+                >
                     RFT Charts
-                </iframe>
+                </iframe> */}
             </Grid>
         </Grid>
             
---
+++ frontend/src/components/pages/RFTchart.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const RFTchart = () => {
+  return (
+    <div>
+        <iframe 
+                    width="900" 
+                    height="695" 
+                    src="https://datastudio.google.com/embed/reporting/5dc4da61-1505-4a7f-af2d-1c983d1810d9/page/7AB0C" 
+                    frameborder="5" 
+                    allowfullscreen
+                    title="Charts" 
+                >
+                    RFT Charts
+                </iframe>
+    </div>
+  )
+}
+
+export default RFTchart;
